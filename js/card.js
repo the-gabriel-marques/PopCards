dce3f45ca8369caff4a157adcc0d8e3a24b02048
the-gabriel-marques <gabrielmarquesdasilvacontato@gmail.com>
2026-02-04 19:48:02 -0300

    PokeAPI Testado

---
+++ js/card.js
@@ -0,0 +1,4 @@
+
+document.getElementById('tituloFilme').innerHTML = 'Reflexões de um liquidificador';
+document.getElementById('direcaoFilme').textContent = 'André Klotzel';
+document.getElementById('anoFilme').textContent = '2010';
